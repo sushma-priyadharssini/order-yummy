743651929d389a2adfa791c1c7ad30786b841bce
Sushma Priyadharssini KV <kvsushmapriyadharssini@gmail.com>
2021-08-29 01:05:38 +0530

    App header set up

---
+++ src/components/layout/header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import foodImage from '../../assets/food.jpg';
+import classes from './header.module.css'
+
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>Order Yummy!</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={foodImage} alt="Food Varieties" />
+            </div>
+        </Fragment>
+    );
+}
+
+export default Header;
