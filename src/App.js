079bc86eeb52cd1e216e1508c0a2e8ae934e5b7f
Sushma Priyadharssini KV <kvsushmapriyadharssini@gmail.com>
2021-08-29 13:13:51 +0530

    Setting up Cart context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import Cart from './components/cart/cart';
 import Header from './components/layout/header';
 import Meals from './components/meals/meals';
+import CartProvider from './store/cartProvider';
 
 
 function App() {
@@ -12,13 +13,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {isCartShown && <Cart cartHandler={toggleShowCardHandler}/>}
       <Header onShowCartHandler={toggleShowCardHandler}/>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
---
+++ src/store/cartProvider.js
@@ -0,0 +1,27 @@
+
+import CartContext from "./cart-context";
+
+const CartProvider = props => {
+
+    const addItemHandler = (item) => {
+
+    }
+
+    const removeItemHandler = (item) => {
+        
+    }
+
+    const cartContext = {
+        items: [],
+        total: 0,
+        addItem: addItemHandler,
+        removeItem: removeItemHandler
+    }
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider;
